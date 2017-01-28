7c30f374b18a6cdbe63b9e16431e48ebc1b11752
Gaby Becker <gaby.becker@condatapool.de>
2017-01-28 17:18:43 +0100

    - changed design of the dialog-fragment to make it conform to google-dialogs

--- app/src/main/java/de/bahnhoefe/deutschlands/bahnhofsfotos/Dialogs/MyDataDialogFragment.java
+++ app/src/main/java/de/bahnhoefe/deutschlands/bahnhofsfotos/Dialogs/MyDataDialogFragment.java
@@ -6,10 +6,17 @@ import android.app.DialogFragment;
 
 import android.content.DialogInterface;
 import android.content.Intent;
+import android.graphics.Color;
+import android.graphics.Typeface;
 import android.os.Bundle;
+import android.util.DisplayMetrics;
+import android.util.Log;
+import android.view.ContextThemeWrapper;
 import android.view.KeyEvent;
 import android.view.LayoutInflater;
 import android.view.View;
+import android.view.WindowManager;
+import android.widget.Button;
 
 import de.bahnhoefe.deutschlands.bahnhofsfotos.DetailsActivity;
 import de.bahnhoefe.deutschlands.bahnhofsfotos.MainActivity;
@@ -23,28 +30,32 @@ import static android.content.DialogInterface.*;
  */
 
 public class MyDataDialogFragment extends DialogFragment {
+    private final String TAG = getClass().getSimpleName();
 
     @Override
     public Dialog onCreateDialog(Bundle savedInstanceState) {
-        LayoutInflater inflater = getActivity().getLayoutInflater();
-        View view = inflater.inflate(R.layout.fragment_mydata_dialog, null);
-        AlertDialog.Builder builder = new AlertDialog.Builder(getActivity());
 
-        builder.setView(view).setPositiveButton("OK", new OnClickListener() {
+        AlertDialog.Builder builder = new AlertDialog.Builder(new ContextThemeWrapper(getActivity(), R.style.AlertDialogCustom));
+
+        builder
+        .setIcon(R.mipmap.ic_launcher)
+        .setTitle(R.string.dialog_title)
+        .setMessage(R.string.dialog_message)
+        .setPositiveButton("OK", new OnClickListener() {
             @Override
             public void onClick(DialogInterface dialog, int which) {
                 Intent myDataIntent = new Intent(getActivity(),MyDataActivity.class);
                 startActivity(myDataIntent);
             }
         })
-        .setNegativeButton("ABBRECHECN", new OnClickListener() {
+        .setNegativeButton("ABBRECHEN", new OnClickListener() {
             @Override
             public void onClick(DialogInterface dialog, int which) {
-                //MyDataDialogFragment.this.getDialog().cancel();
                 Intent mainIntent = new Intent(getActivity(), MainActivity.class);
                 startActivity(mainIntent);
             }
         });
         return builder.create();
     }
+
 }
