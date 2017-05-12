ff6d537758132fc66f190b3a633ea5e6a06b0ad0
Peter Storch <peter.storch@gmail.com>
2017-05-12 20:08:15 +0200

    location updates off on nearest map

--- app/src/main/java/de/bahnhoefe/deutschlands/bahnhofsfotos/MapsActivity.java
+++ app/src/main/java/de/bahnhoefe/deutschlands/bahnhofsfotos/MapsActivity.java
@@ -13,10 +13,8 @@ import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.support.v4.app.ActivityCompat;
-import android.support.v7.app.ActionBar;
 import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
-
 import android.support.v7.widget.Toolbar;
 import android.util.Log;
 import android.view.LayoutInflater;
@@ -73,7 +71,7 @@ public class MapsActivity extends AppCompatActivity implements OnMapReadyCallbac
      * Stores parameters for requests to the FusedLocationProviderApi.
      */
     private LocationRequest mLocationRequest;
-    private boolean mRequestingLocationUpdates = true;
+    private boolean mRequestingLocationUpdates = false;
     private BahnhofsDbAdapter dbAdapter;
 
     @Override
