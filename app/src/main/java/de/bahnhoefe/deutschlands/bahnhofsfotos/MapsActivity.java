dc3f8df4cad99d3a42740891354142ba7c93d602
Gaby Becker <gaby.becker@condatapool.de>
2017-07-23 12:05:30 +0200

    Changed color of the red marker, changed Menu-titles

--- app/src/main/java/de/bahnhoefe/deutschlands/bahnhofsfotos/MapsActivity.java
+++ app/src/main/java/de/bahnhoefe/deutschlands/bahnhofsfotos/MapsActivity.java
@@ -225,7 +225,7 @@ public class MapsActivity extends AppCompatActivity implements OnMapReadyCallbac
 
     private BitmapDescriptor getMarkerIcon(Bahnhof bahnhof, String nickname) {
         if (!bahnhof.hasPhoto()) {
-            return BitmapDescriptorFactory.defaultMarker(BitmapDescriptorFactory.HUE_RED);
+            return BitmapDescriptorFactory.defaultMarker(343);
         } else if (bahnhof.getPhotographer().equals(nickname)) {
             return BitmapDescriptorFactory.defaultMarker(BitmapDescriptorFactory.HUE_BLUE);
         }
