1059b434e21b8e5e081b9f961edf5db5042ecc65
Peter Storch <peter.storch@gmail.com>
2020-10-04 14:57:12 +0200

    add missing problem types

--- app/src/main/java/de/bahnhoefe/deutschlands/bahnhofsfotos/model/ProblemType.java
+++ app/src/main/java/de/bahnhoefe/deutschlands/bahnhofsfotos/model/ProblemType.java
@@ -4,7 +4,9 @@ import de.bahnhoefe.deutschlands.bahnhofsfotos.R;
 
 public enum ProblemType {
     WRONG_LOCATION(R.string.problem_wrong_location),
+    WRONG_NAME(R.string.problem_wrong_name),
     STATION_INACTIVE(R.string.problem_station_inactive),
+    STATION_ACTIVE(R.string.problem_station_active),
     STATION_NONEXISTENT(R.string.problem_station_nonexistent),
     WRONG_PHOTO(R.string.problem_wrong_photo),
     OTHER(R.string.problem_other);
