c66ceacf68431c265fd13b0fbdce27220702f82f
Peter Storch <peter.storch@tuta.io>
2023-05-27 21:55:02 +0200

    Add test for photoUpload and problemReport

--- app/src/test/java/de/bahnhoefe/deutschlands/bahnhofsfotos/rsapi/RSAPIClientTest.java
+++ app/src/test/java/de/bahnhoefe/deutschlands/bahnhofsfotos/rsapi/RSAPIClientTest.java
@@ -7,18 +7,25 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import java.net.URL;
+import java.net.URLConnection;
+import java.nio.charset.Charset;
 import java.util.List;
 import java.util.Objects;
 
 import de.bahnhoefe.deutschlands.bahnhofsfotos.model.Country;
 import de.bahnhoefe.deutschlands.bahnhofsfotos.model.HighScoreItem;
+import de.bahnhoefe.deutschlands.bahnhofsfotos.model.InboxResponse;
 import de.bahnhoefe.deutschlands.bahnhofsfotos.model.Photo;
 import de.bahnhoefe.deutschlands.bahnhofsfotos.model.PhotoLicense;
 import de.bahnhoefe.deutschlands.bahnhofsfotos.model.PhotoStation;
 import de.bahnhoefe.deutschlands.bahnhofsfotos.model.PhotoStations;
 import de.bahnhoefe.deutschlands.bahnhofsfotos.model.Photographer;
+import de.bahnhoefe.deutschlands.bahnhofsfotos.model.ProblemReport;
+import de.bahnhoefe.deutschlands.bahnhofsfotos.model.ProblemType;
 import de.bahnhoefe.deutschlands.bahnhofsfotos.model.ProviderApp;
 import de.bahnhoefe.deutschlands.bahnhofsfotos.model.Statistic;
+import okhttp3.MediaType;
+import okhttp3.RequestBody;
 import okhttp3.mockwebserver.MockResponse;
 import okhttp3.mockwebserver.MockWebServer;
 import okio.Buffer;
@@ -208,6 +215,43 @@ class RSAPIClientTest {
                                                 1451846273000L,
                                                 "CC0_10")
                                 )))));
+    }
+
+    @Test
+    void postPhotoUpload() throws Exception {
+        server.enqueue(new MockResponse().setBody(fromFile("photoUpload.json")));
+
+        var requestBody = RequestBody.create("IMAGE".getBytes(), MediaType.parse(URLConnection.guessContentTypeFromName("photo.png")));
+        var response = client.photoUpload("4711", "de", "Title", 50d, 9d, "Comment", null, requestBody).execute();
+
+        var recordedRequest = server.takeRequest();
+        assertThat(recordedRequest.getPath()).isEqualTo("/photoUpload");
+        assertThat(recordedRequest.getHeader("Station-Id")).isEqualTo("4711");
+        assertThat(recordedRequest.getHeader("Country")).isEqualTo("de");
+        assertThat(recordedRequest.getHeader("Station-Title")).isEqualTo("Title");
+        assertThat(recordedRequest.getHeader("Latitude")).isEqualTo("50.0");
+        assertThat(recordedRequest.getHeader("Longitude")).isEqualTo("9.0");
+        assertThat(recordedRequest.getHeader("Comment")).isEqualTo("Comment");
+        assertThat(recordedRequest.getHeader("Active")).isNull();
+        assertThat(recordedRequest.getHeader("Content-Type").split(";")[0]).isEqualTo("image/png");
+        assertThat(recordedRequest.getBody().readString(Charset.defaultCharset())).isEqualTo("IMAGE");
+        assertThat(response.body()).isNotNull();
+        assertThat(response.body()).usingRecursiveComparison().isEqualTo(new InboxResponse(InboxResponse.InboxResponseState.REVIEW, "message", 123456L, "123456.png", "https://localhost:8080/inbox/123456.png", 4711L));
+    }
+
+    @Test
+    void postProblemReport() throws Exception {
+        server.enqueue(new MockResponse().setBody(fromFile("reportProblem.json")));
+
+        var problemReport = new ProblemReport("de", "4711", "Comment", ProblemType.WRONG_PHOTO, 123456L, null, null);
+        var response = client.reportProblem(problemReport).execute();
+
+        var recordedRequest = server.takeRequest();
+        assertThat(recordedRequest.getPath()).isEqualTo("/reportProblem");
+        assertThat(recordedRequest.getHeader("Content-Type")).isEqualTo("application/json");
+        assertThat(recordedRequest.getBody().readString(Charset.defaultCharset())).isEqualTo("{\"countryCode\":\"de\",\"stationId\":\"4711\",\"comment\":\"Comment\",\"type\":\"WRONG_PHOTO\",\"photoId\":123456}");
+        assertThat(response.body()).isNotNull();
+        assertThat(response.body()).usingRecursiveComparison().isEqualTo(new InboxResponse(InboxResponse.InboxResponseState.REVIEW, "message", 123456L));
     }
 
     Buffer fromFile(final String filename) throws Exception {
